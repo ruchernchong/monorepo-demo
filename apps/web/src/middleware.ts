518f7d77ac7a3f1ab2606af235aa97bf083c8116
Ru Chern Chong <hello@ruchern.dev>
2025-11-23 14:15:19 +0800

    Only either middleware.ts or proxy.ts is allowed

--- apps/web/src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { type NextRequest, NextResponse } from "next/server";
-
-const middleware = (request: NextRequest) => {
-  console.log(request.nextUrl.pathname);
-  return NextResponse.next();
-};
-
-export default middleware;
